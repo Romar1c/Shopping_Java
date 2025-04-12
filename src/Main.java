aaf2e98d6ca652ece032315ad2d6269f244cc01d
Romaric BARBAUD <romaric.barbaud@edu.ece.fr>
2025-04-13 01:02:56 +0200

    Changement Affichage

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
 import shop.vue.FenetreConnexion;
 
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        new FenetreConnexion();
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    new FenetreConnexion();
+                }
+            });
+        } catch (Exception ex) {
+            // Erreur de LookAndFeel
+            ex.printStackTrace();
+        }
+
+
     }
 }
--- src/shop/Principal.java
+++ src/shop/Principal.java
@@ -1,9 +1,10 @@
 package shop;
 
+import shop.vue.FenetreConnexion;
 import shop.vue.FenetreInscription;
 
 public class Principal {
     public static void main(String[] args) {
-        new FenetreInscription();
+        new FenetreConnexion();
     }
 }
